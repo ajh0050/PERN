e0b089579cec03d7c3b75ed3142581850ee35251
Adam Hughes <adamjordanhughes@gmail.com>
2023-05-30 17:09:45 -0400

    Add basic middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
+
+//middleware
+app.use(cors())
+app.use(express.json())
 
 app.listen(4000, () => {
     console.log("The port is live and listening at port 4000")
